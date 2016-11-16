fd32efa21176f72d95003ea6de1dcb1919b3febf
Marty O <Olson@Martins-iMac.local>
2016-11-16 10:25:55 -0600

    Delete Row - Working

--- myscript.js
+++ myscript.js
@@ -1,7 +1,7 @@
 var invObj = [];
 
 // Initialization on page load - If data exists in database (localStorage for testing), build the Inventory Table.
-// Loop through and assign Item Index Number to handle deletion / editing of items.
+
 function init() {
   if (localStorage.foodInv) {
     invObj = JSON.parse(localStorage.foodInv);
@@ -10,10 +10,10 @@ function init() {
 };
 
 // Loop through inventory object to build the inventory table.
-//var delIcon = '<button id="delFood"><i class="fa fa-trash-o" aria-hidden="true"></i></button>';
 
 function buildInv() {
   var tableContent = document.getElementById("myInv").getElementsByTagName("tbody")[0];
+  tableContent.innerHTML = '';
   for (i = 0; i < invObj.length; i++) {
     // Assign index number to each entry for deletion / editing
     invObj[i].index = i;
@@ -29,18 +29,24 @@ function buildInv() {
     var expCell = newRow.insertCell(2);
     var delCell = newRow.insertCell(3);
 
-
     foodCell.innerHTML = foodName;
     amtCell.innerHTML = amt + ' ' + lbl;
     expCell.innerHTML = expiry;
-    delCell.innerHTML = '<button class="trash" id="' + i + '"><i class="fa fa-trash-o" aria-hidden="true"></i></button>';
+
+    var delButton = document.createElement('button');
+    delButton.setAttribute('id', i);
+    delButton.setAttribute('class', 'trash');
+    delButton.innerHTML = '<i class="fa fa-trash-o" aria-hidden="true"></i>';
+    delCell.append(delButton);
+    delButton.addEventListener('click', delFunc);
   };
 };
+
+// Push user entered item into inventory and rebuild the inventory table.
 
 var submit = document.getElementById("addNew");
 submit.addEventListener("click", addItem);
 
-// Push user entered item into inventory and rebuild the inventory table.
 function addItem() {
   var addFood = document.getElementById('newFood').value;
   var addAmt = document.getElementById('newAmt').value;
@@ -57,21 +63,20 @@ function addItem() {
   buildInv();
   var inv = JSON.stringify(invObj);
   localStorage.foodInv = inv;
-
 };
 
 // Delete Item From Inventory Using Trash Button
-var deleteFood = document.querySelector('tbody');
-deleteFood.addEventListener('click', delFunc);
 
 function delFunc() {
-    var currEle = event.target;
-    console.log(currEle);
-    var currId = currEle.id;
-    console.log(currId);
-    var loc = parseInt(currId);
-    console.log(loc);
-    invObj = invObj.splice(loc, 1);
-    console.log(invObj);
-    buildInv();
+  var currEle = event.target;
+  var currId = currEle.id;
+  // If user clicks on trash icon instead of surrounding button, assign id of the button.
+  if (!currId) {
+    currId = currEle.parentElement.id;
+  };
+  var loc = parseInt(currId);
+  invObj.splice(loc, 1);
+  buildInv();
+  var inv = JSON.stringify(invObj);
+  localStorage.foodInv = inv;
 };
